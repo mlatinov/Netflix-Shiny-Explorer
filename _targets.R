cedb56720914659b442582df8f03814347b53cc2
mlatinov <latinovmetodi@gmail.com>
2025-07-28 02:13:27 +0900

    # finish the forecast cycle

--- _targets.R
+++ _targets.R
@@ -13,6 +13,7 @@ library(tidymodels)
 library(finetune)
 library(modeltime)
 library(modeltime.resample)
+library(modeltime.ensemble)
 
 ## Viz ##
 library(plotly)
@@ -79,10 +80,100 @@ list(
   ),
   ## PROPHET model ##
   tar_target(
-    name = prophet_model,
+    name = prophet_boost_model,
     command = prophet_model_function
-  )
+  ),
+  ## Deep Ar ##
+  ## GP Forecaster ##
 
+  ## Modeltime Table ##
+  tar_target(
+    name = modeltime_table,
+    command = modeltime_table(
+      arima_boost_model,
+      prophet_boost_model
+    )
+  ),
+  ## Ensemble ##
+  tar_target(
+    name = ts_ensemble,
+    command =  ensemble_average(
+      object = modeltime_table,
+      type = "mean"
+      )
+    ),
+## Calibration ##
+tar_target(
+  name = calibration_table,
+  command = modeltime_calibrate(
+    object = modeltime_table,
+    new_data = testing(ts_split)
+    )
+  ),
+## Ensemble Calibration ##
+tar_target(
+  name = calibration_table_ensemble,
+  command = modeltime_calibrate(
+    object = ts_ensemble,
+    new_data = testing(ts_split)
+  )
+),
+  ## Forecast vs Test Set ##
+  tar_target(
+    name = forecast_test_set_check,
+    command = modeltime_forecast(
+      object = calibration_table,
+      new_data = testing(ts_split),
+      actual_data = netflix_stock_price,
+      conf_interval = 0.95
+      )
+    ),
+## Forecast vs Test Set Ensemble  ##
+tar_target(
+  name = forecast_test_set_check_ensemble,
+  command = modeltime_forecast(
+    object = calibration_table_ensemble,
+    new_data = testing(ts_split),
+    actual_data = netflix_stock_price,
+    conf_interval = 0.95
+    )
+),
+  ## Refit to Full Dataset & Forecast Forward ##
+  tar_target(
+    name = refit,
+    command = modeltime_refit(
+      object = calibration_table,
+      data = netflix_stock_price
+      )
+    ),
+## Refit Ensemble ##
+tar_target(
+  name = refit_ensemble,
+  command = modeltime_refit(
+    object = calibration_table_ensemble,
+    data = netflix_stock_price
+  )
+),
+## Forecast Forward ##
+tar_target(
+  name = forecast_forward,
+  command = modeltime_forecast(
+    object = refit,
+    new_data = ts_extent,
+    actual_data = netflix_stock_price,
+    conf_interval = 0.95
+    )
+  ),
+## Forecast Forward Ensemble ##
+tar_target(
+  name = forecast_forward_ensemble,
+  command = modeltime_forecast(
+    object = refit_ensemble,
+    new_data = ts_extent,
+    actual_data = netflix_stock_price,
+    conf_interval = 0.95
+    )
+  ),
 )
 
 
