07049ff3ba7c37e5f1753294422058e37a9b10e5
mlatinov <latinovmetodi@gmail.com>
2025-07-27 23:45:04 +0900

    prophet function

--- functions/ts_functions.R
+++ functions/ts_functions.R
@@ -5,7 +5,7 @@
 prepare_ts <- function(data){
   
   ## Create a recipe for preprocesing ##
-  recipe_ts <- recipe(close ~ .,data = training(data)) %>%
+  recipe <- recipe(close ~ .,data = training(data)) %>%
     
     # Convert date to time-based features
     step_timeseries_signature(date) %>%
@@ -23,11 +23,12 @@ prepare_ts <- function(data){
     )
   
   # Return the recipe 
-  return(recipe_ts)
+  return(recipe)
 }
 
 #### Model Function ####
 
+## Arima Model ##
 arima_model_function <- function(ts_split,ts_recipe){
   
   ## Arima Spec ##
@@ -97,5 +98,81 @@ arima_model_function <- function(ts_split,ts_recipe){
   return(final_wf)
 }
 
-
+## Prophet Model ## 
+prophet_model_function <- function(ts_split,ts_recipe){
+  
+  # Prophet Spec
+  prophet_spec <- prophet_boost(
+    mode = "regression",
+    mtry = tune(),
+    min_n = tune(),
+    tree_depth = tune(),
+    trees = tune(),
+    learn_rate = tune(),
+    changepoint_num = tune(),
+    changepoint_range = tune()  
+    ) %>%
+    set_engine("prophet_xgboost")
+  # Prophet Workflow 
+  prophet_workflow <- workflow() %>%
+    add_model(prophet_spec) %>%
+    add_recipe(ts_recipe)
+  # Resamples
+  resamples_ts <- time_series_cv(
+    data = training(ts_split),
+    date_var = date,
+    initial = 365 * 3,   
+    assess = 365 * 1,    
+    slice_limit = 10     
+  )
+  ## LHC Tune Grid
+  # Define a param set 
+  param_set <- parameters(
+    mtry(range = c(5, 30)),
+    min_n(range = c(10, 80)),
+    tree_depth(range = c(1, 10)),
+    trees(range = c(500, 1500)),
+    learn_rate(range = c(1e-10, 1e-1), trans = log10_trans()),
+    changepoint_num(range = c(5, 50)),
+    changepoint_range(range = c(0.5, 0.95))
+  )
+  # Generate Latin Hypercube grid
+  lch_tune_grid <- grid_space_filling(
+    param_set,
+    size = 35
+  )
+  # Metric 
+  metric <- metric_set(rmse)
+  # Initial tuning
+  initial_results <- tune_grid(
+    prophet_workflow,
+    resamples = resamples_ts,
+    grid = lch_tune_grid,
+    metrics = metric,
+    control = control_grid(save_pred = TRUE)
+  )
+  # MBO Control 
+  control <- control_bayes(
+    no_improve = 20,
+    seed = 123,
+    save_pred = TRUE,
+    save_workflow = TRUE
+    )
+  # Tune MBO
+  tune_mbo <- tune_bayes(
+    prophet_workflow,
+    resamples = resamples_ts,
+    iter = 50,
+    param_info = param_set,
+    metrics = metric,
+    initial = initial_results,
+    control = control
+  )
+  # Select Best 
+  best_tune <- select_best(tune_mbo)
+  # Finalize the workflow 
+  prophet_final_wf <- finalize_workflow(prophet_workflow,parameters = best_tune)
+  # Return final workflow 
+  return(prophet_final_wf)
+}
 
